c7385a309d4aeab8c235bc5f0222963fd8428843
Lucy Vin <lucy@lucyvin.com>
2019-12-16 16:58:37 -0500

    fix formatting in mirror.sh

--- .mirror.sh
+++ .mirror.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X PUT -H 'Accept: application/vnd.github.barred-rock-preview' -H "authToken: $GITHUB_AUTH" --data "{vcs: git, vcs_url    : $CANNONICAL_MAIN_REPO}" https://api.github.com/repos/lcyvin/ephemeral.work/import
+curl -X PUT -H 'Accept: application/vnd.github.barred-rock-preview' -H "authToken: $GITHUB_AUTH" --data "{vcs: git, vcs_url: $CANNONICAL_MAIN_REPO}" https://api.github.com/repos/lcyvin/ephemeral.work/import
